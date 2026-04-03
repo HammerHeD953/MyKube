bdc43a5fdfc65a9259ccd938c705478fc16a527b
HammerHeD953 <HammerHeD953@gmail.com>
2026-04-03 20:43:11 +0300

    Delete_Secret_key

--- makeuser-ubuntu.sh
+++ makeuser-ubuntu.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 username=ansible
 sshkey="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEmegYSWcme2WLRF4t2ZFCq+bzoM+q0Ed6LpKsRdQrS7 games@Gamer-PC"
-id_ed25519="-----BEGIN OPENSSH PRIVATE KEY-----
-b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtzc2gtZW
-QyNTUxOQAAACBJnoGElnJntli0ReLdmRQqvm86DPqtBHei6SrEXUK0uwAAAJjrBtql6wba
-pQAAAAtzc2gtZWQyNTUxOQAAACBJnoGElnJntli0ReLdmRQqvm86DPqtBHei6SrEXUK0uw
-AAAEAsrmTlWOHfiGyjH8FsD0ZJBEgXX5N2BqPm6cMRbZsx0UmegYSWcme2WLRF4t2ZFCq+
-bzoM+q0Ed6LpKsRdQrS7AAAADmdhbWVzQEdhbWVyLVBDAQIDBAUGBw==
------END OPENSSH PRIVATE KEY-----"
+id_ed25519=""
 id_ed25519_pub="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEmegYSWcme2WLRF4t2ZFCq+bzoM+q0Ed6LpKsRdQrS7 games@Gamer-PC"
 useradd -m -d /home/$username -s /bin/bash $username
 mkdir -p /home/$username/.ssh && touch /home/$username/.ssh/authorized_keys && touch /home/$username/.ssh/id_ed25519 && touch /home/$username/.ssh/id_ed25519.pub
